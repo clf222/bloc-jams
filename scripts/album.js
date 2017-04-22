69aa493516bcd658c253f5990536ef8f077071d3
clfrancies <clfrancies@verizon.net>
2017-04-22 09:45:13 -0400

    Checkpoint 26: completed

--- scripts/album.js
+++ scripts/album.js
@@ -1,41 +1,58 @@
 //Example Album 
 
 var albumPicasso = {
-  title: 'The Colors',
-  artist: 'Pablo Picasso',
-  label: 'Cubism',
-  year: '1881',
-  albumArtUrl: 'assets/images/album_covers/01.png',
-  songs: [
-      { title: 'Blue', duration: '4:26' },
-      { title: 'Green', duration: '3:14' },
-      { title: 'Red', duration: '5:01' },
-      { title: 'Pink', duration: '3:21'},
-      { title: 'Magenta', duration: '2:15'}
-   ]
-};
+        title: 'The Colors',
+        artist: 'Pablo Picasso',
+        label: 'Cubism',
+        year: '1881',
+        albumArtUrl: 'assets/images/album_covers/01.png',
+        songs: [
+            { title: 'Blue', duration: '4:26' },
+            { title: 'Green', duration: '3:14' },
+            { title: 'Red', duration: '5:01' },
+            { title: 'Pink', duration: '3:21'},
+            { title: 'Magenta', duration: '2:15'}
+        ]
+    };
 
 //Another Example Album
 
 var albumMarconi = {
-  title: 'The Telephone',
-  artist: 'Guglielmo Marconi',
+        title: 'The Telephone',
+        artist: 'Guglielmo Marconi',
+        label: 'EM',
+        year: '1909',
+        albumArtUrl: 'assets/images/album_covers/20.png',
+        songs: [
+            { title: 'Hello, Operator?', duration: '1:01' },
+            { title: 'Ring, ring, ring', duration: '5:01' },
+            { title: 'Fits in your pocket', duration: '3:21'},
+            { title: 'Can you hear me now?', duration: '3:14'},
+            { title: 'Wrong phone number', duration: '2:15'}
+        ]
+    };
+//My album object
+var albumMuteMath = {
+  title: 'Vitals',
+  artist: 'MuteMath',
   label: 'EM',
-  year: '1909',
-  albumArtUrl: 'assets/images/album_covers/20.png',
+  year: '2016',
+  albumArtUrl: 'assets/images/album_covers/03.png',
   songs: [
-      { title: 'Hello, Operator?', duration: '1:01' },
-      { title: 'Ring, ring, ring', duration: '5:01' },
-      { title: 'Fits in your pocket', duration: '3:21'},
-      { title: 'Can you hear me now?', duration: '3:14'},
-      { title: 'Wrong phone number', duration: '2:15'}
+      { title: 'Monument', duration: '1:01' },
+      { title: 'Light Up', duration: '5:01' },
+      { title: 'Joy Rides', duration: '3:21'},
+      { title: 'Best of Intentions', duration: '3:14'},
+      { title: 'Used To', duration: '2:15'}
     ]
 };
+
+
   
 var createSongRow = function(songNumber, songName, songLength) {
   var template = 
     '<tr class="album-view-song-item">'
-  + '  <td class="song-item-number">' + songNumber + '</td>'
+  + '  <td class="song-item-number" data-song-number="'  + songNumber + '">'+ songNumber + '</td>'
   + '  <td class="song-item-title">'  + songName   + '</td>'
   + '  <td class="song-item-duration">' + songLength + '</td>'
   + '</tr>'
@@ -66,12 +83,39 @@ var setCurrentAlbum = function(album) {
   }
   
 };
+
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item')
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span>></a>';
+
   window.onload = function() {
     setCurrentAlbum(albumPicasso);
+      songListContainer.addEventListener('mouseover', function(event){
+         if (event.target.parentElement.className === 'album-view-song-item'){
+             event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+         }
+      });
+    for (var i = 0; i < songRows.length; i++) {
+        songRows[i].addEventListener('mouseleave', function(event){
+            this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+            
+        });
+    }
   };
   
-  
-  
+ //writing event listener
+ //add listener to image
+//put album titles in an array
+//on click, album title = ablum title +1 
+//if i = array. length, album title = 0;
+//call setCurrentAlbum(title(i));
+//  
+//function changeMe() {
+//    
+//    
+//    
+//}
+//  
+//  
 
-  
-  
+
